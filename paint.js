67d48cf802566bfcc9d8e004adb7c3fa4cd7fa0d
lionel <staricofflionel@hotmail.com>
2023-04-19 12:05:41 -0300

    funcionando el sistema de dibujo por puntos desde js

---
+++ paint.js
@@ -0,0 +1,28 @@
+let pantalla = document.querySelector("canvas");
+let pincel = pantalla.getContext("2d");
+
+pincel.fillStyle = "grey";
+pincel.fillRect(0,0,600,400);
+
+
+//evento: se refiere al mouse cuando hace click
+//esta funcion indica en que jugar de la pantalla hace click el mouse
+function exibirAlerta(evento){
+    let x = evento.pageX - pantalla.offsetLeft; //resta de la pantalla lo que no pertenece al canvas del lado izquierdo como el margin
+    let y = evento.pageY - pantalla.offsetTop;
+    alert(x + ", " + y);
+}
+
+
+function dibujarCirculo(evento){
+    let x = evento.pageX - pantalla.offsetLeft;
+    let y = evento.pageY - pantalla.offsetTop;
+
+    pincel.fillStyle = "blue";
+    pincel.beginPath();
+    pincel.arc(x,y,10,0,2*Math.PI);
+    pincel.fill();
+   // console.log(x + ", "+y);
+
+}
+pantalla.onclick = dibujarCirculo;
